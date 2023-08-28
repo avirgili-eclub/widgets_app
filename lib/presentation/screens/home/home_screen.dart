4893417c9fba883e0c6d15573f95852da1f6b9d1
avirgili <avirgilitech@gmail.com>
2023-08-27 21:45:59 -0400

    added screens and basic routes for navigation

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -46,8 +47,10 @@ class _CustomListTitle extends StatelessWidget {
       iconColor: colors.primary,
       trailing: const Icon(Icons.arrow_forward_ios_rounded),
       onTap: () {
-        //TODO: Implementar navegación
-        // Navigator.pushNamed(context, menuItem.link);
+        // Navigator.of(context).push(
+        //   MaterialPageRoute(builder: (context) => const ButtonsScreens()),
+        // );
+        Navigator.pushNamed(context, menuItem.link);
       },
     );
   }
